5f929c3a970305061a215a44ac19b2cffc137e54
acbc <acbc.dev@gmail.com>
2025-07-05 10:22:49 -0500

    feat: refactor App component to use Layout and implement routing with RouterProvider

--- entrypoints/dashboard/main.tsx
+++ entrypoints/dashboard/main.tsx
@@ -1,11 +1,18 @@
+import { createHashRouter, RouterProvider } from "react-router";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "@/assets/tailwind.css";
-import { Toaster } from "@/components/ui/sonner";
+
+let router = createHashRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <div>Not Found</div>,
+  },
+]);
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-    <Toaster />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
