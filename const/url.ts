d780ade06bbfd3984c68302e9807f7a0e03c16a6
acbc <acbc.dev@gmail.com>
2025-07-16 13:47:53 -0500

    new feuture

--- const/url.ts
+++ const/url.ts
@@ -1,1 +1,4 @@
 export const CHATURBATE_TAGS_URL = "https://chaturbate.com/tags";
+export const SODAHOURS_URL = "https://www.sodahours.com/user";
+export const STRIPHOURS_URL = "https://www.striphours.com/user";
+export const CBHOURS = "https://www.cbhours.com/user/";
