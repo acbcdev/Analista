72e743ee6425abee1d7e602e1727e0213533f552
acbc <acbc.dev@gmail.com>
2025-07-17 11:31:51 -0500

    improve sidebar

--- entrypoints/dashboard/components/layout/appSidebar.tsx
+++ entrypoints/dashboard/components/layout/appSidebar.tsx
@@ -1,110 +1,114 @@
-"use client";
-
-import * as React from "react";
+import { Title } from "@radix-ui/react-dialog";
 import {
-	ChartArea,
-	Hourglass,
-	LayoutDashboard,
-	TableProperties,
-	Tags,
+  ChartArea,
+  Hourglass,
+  LayoutDashboard,
+  TableProperties,
+  Tags,
+  Users,
 } from "lucide-react";
-
+import type * as React from "react";
+import { Link, useLocation } from "react-router";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarRail,
+  SidebarSeparator,
+} from "@/components/ui/sidebar";
 import { NavMain } from "@/entrypoints/dashboard/components/layout/navMain";
 import { TeamSwitcher } from "@/entrypoints/dashboard/components/layout/teamSwitcher.tsx";
-import {
-	Sidebar,
-	SidebarContent,
-	SidebarGroup,
-	SidebarHeader,
-	SidebarMenu,
-	SidebarMenuButton,
-	SidebarMenuItem,
-	SidebarRail,
-	SidebarSeparator,
-} from "@/components/ui/sidebar";
 import { useModelsStore } from "@/store/models";
-import { Link, useLocation } from "react-router";
 
 const data = {
-	user: {
-		name: "shadcn",
-		email: "m@example.com",
-		avatar: "/avatars/shadcn.jpg",
-	},
-	// teams: [
-	//   {
-	//     name: "admin",
-	//     icon: ShieldUser,
-	//   },
-	//   {
-	//     name: "Acme Inc",
-	//     icon: "🥑",
-	//     site: "Orange",
-	//   },
-	// ],
-	navMain: [
-		{
-			title: "Dashboard",
-			url: "/",
-			icon: LayoutDashboard,
-		},
-		{
-			title: "Analytics",
-			url: "/analytics",
-			icon: ChartArea,
-		},
-		{
-			title: "streams",
-			url: "/streams",
-			icon: TableProperties,
-		},
-	],
+  user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  // teams: [
+  //   {
+  //     name: "admin",
+  //     icon: ShieldUser,
+  //   },
+  //   {
+  //     name: "Acme Inc",
+  //     icon: "🥑",
+  //     site: "Orange",
+  //   },
+  // ],
+  navMain: [
+    {
+      title: "Dashboard",
+      url: "/",
+      icon: LayoutDashboard,
+    },
+    {
+      title: "Analytics",
+      url: "/analytics",
+      icon: ChartArea,
+    },
+    {
+      title: "streams",
+      url: "/streams",
+      icon: TableProperties,
+    },
+    {
+      title: "Models",
+      url: "/models",
+      icon: Users,
+    },
+  ],
 };
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
-	const models = useModelsStore((state) => state.models);
-	const { pathname } = useLocation();
-	return (
-		<Sidebar collapsible="icon" {...props}>
-			<SidebarHeader>
-				<TeamSwitcher models={models} />
-			</SidebarHeader>
-			<SidebarContent className="overflow-x-hidden">
-				<NavMain items={data.navMain} pathname={pathname} />
-				<SidebarSeparator />
-				<SidebarGroup>
-					<SidebarMenu>
-						<SidebarMenuItem>
-							<Link to="/viewtags">
-								<SidebarMenuButton
-									variant={pathname === "/viewtags" ? "outline" : "default"}
-									tooltip={"View Tags"}
-								>
-									<Tags />
-									View Tags
-								</SidebarMenuButton>
-							</Link>
-						</SidebarMenuItem>
-						<SidebarMenuItem>
-							<Link to="/viewhours">
-								<SidebarMenuButton
-									variant={pathname === "/viewhours" ? "outline" : "default"}
-									tooltip={"View Hours"}
-								>
-									<Hourglass />
-									View Hours
-								</SidebarMenuButton>
-							</Link>
-						</SidebarMenuItem>
-					</SidebarMenu>
-				</SidebarGroup>
+  const models = useModelsStore((state) => state.models);
+  const { pathname } = useLocation();
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader>
+        <TeamSwitcher models={models} />
+      </SidebarHeader>
+      <SidebarContent className="overflow-x-hidden">
+        <NavMain items={data.navMain} pathname={pathname} />
+        <SidebarSeparator />
+        <SidebarGroup>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <Link to="/viewtags">
+                <SidebarMenuButton
+                  variant={pathname === "/viewtags" ? "outline" : "default"}
+                  tooltip={"View Tags"}
+                >
+                  <Tags />
+                  View Tags
+                </SidebarMenuButton>
+              </Link>
+            </SidebarMenuItem>
+            <SidebarMenuItem>
+              <Link to="/viewhours">
+                <SidebarMenuButton
+                  variant={pathname === "/viewhours" ? "outline" : "default"}
+                  tooltip={"View Hours"}
+                >
+                  <Hourglass />
+                  View Hours
+                </SidebarMenuButton>
+              </Link>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarGroup>
 
-				{/* <NavProjects projects={data.projects} /> */}
-			</SidebarContent>
-			{/* <SidebarFooter>
+        {/* <NavProjects projects={data.projects} /> */}
+      </SidebarContent>
+      {/* <SidebarFooter>
         <NavUser />
       </SidebarFooter> */}
-			<SidebarRail />
-		</Sidebar>
-	);
+      <SidebarRail />
+    </Sidebar>
+  );
 }
--- entrypoints/dashboard/routes.tsx
+++ entrypoints/dashboard/routes.tsx
@@ -1,22 +1,22 @@
 import { createHashRouter } from "react-router";
 import App from "./App";
-import { TagsView } from "./routes/ViewTags/page";
 import { HoursView } from "./routes/viewHours/page";
+import { TagsView } from "./routes/viewTags/page";
 
-export let router = createHashRouter([
-	{
-		path: "/",
-		element: <App />,
-		errorElement: <div className="h-dvh w-full">Error ll</div>,
-	},
-	{
-		path: "/viewtags",
-		element: <TagsView />,
-		errorElement: <div className="h-dvh w-full">Error ll</div>,
-	},
-	{
-		path: "/viewhours",
-		element: <HoursView />,
-		errorElement: <div className="h-dvh w-full">Error ll</div>,
-	},
+export const router = createHashRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <div className="h-dvh w-full">Error</div>,
+  },
+  {
+    path: "/viewtags",
+    element: <TagsView />,
+    errorElement: <div className="h-dvh w-full">Error </div>,
+  },
+  {
+    path: "/viewhours",
+    element: <HoursView />,
+    errorElement: <div className="h-dvh w-full">Error </div>,
+  },
 ]);
