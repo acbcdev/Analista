32669e9fe160b8f7d6164b10cb419ec70aa5b53e
acbc <acbc.dev@gmail.com>
2025-07-27 11:35:30 -0500

    feat: add context menu option to get tags in content script for Chaturbate

--- src/entrypoints/content.ts
+++ src/entrypoints/content.ts
@@ -1,8 +1,13 @@
 import { onMessage } from "webext-bridge/content-script";
 
 export default defineContentScript({
-	matches: ["*://*.chaturbate.com/*"],
-	main() {
-		onMessage("open-dashboard", () => {});
-	},
+  matches: ["*://*.chaturbate.com/*"],
+  main() {
+    onMessage("open-dashboard", () => {});
+    browser.contextMenus.create({
+      id: "get-tags",
+      title: "Get Tags",
+      contexts: ["all"],
+    });
+  },
 });
